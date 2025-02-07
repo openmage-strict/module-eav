177e3a6292cbe14d766c208c73460f74d57a9a69
DDEV User <nobody@example.com>
2025-02-07 12:36:11 +0100

    update

--- src/app/code/core/Mage/Eav/Model/Resource/Entity/Attribute/Option/Swatch.php
+++ src/app/code/core/Mage/Eav/Model/Resource/Entity/Attribute/Option/Swatch.php
@@ -9,14 +9,20 @@
  *
  * @category   Mage
  * @package    Mage_Eav
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2020-2024 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
+ * Entity attribute swatch resource model
+ *
  * @category   Mage
  * @package    Mage_Eav
- * @deprecated
  */
-class Mage_Eav_Model_Mysql4_Entity_Type_Collection extends Mage_Eav_Model_Resource_Entity_Type_Collection {}
+class Mage_Eav_Model_Resource_Entity_Attribute_Option_Swatch extends Mage_Core_Model_Resource_Db_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('eav/attribute_option_swatch', 'option_id');
+    }
+}
